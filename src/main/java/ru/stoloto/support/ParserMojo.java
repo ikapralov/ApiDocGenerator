98d23fd35203cbacd2f42d41ab8a527005ad0795
ikapralov <ikapralov88@yandex.ru>
2018-03-08 13:06:15 +0300

    xvc

--- src/main/java/ru/stoloto/support/ParserMojo.java
+++ src/main/java/ru/stoloto/support/ParserMojo.java
@@ -2,26 +2,32 @@ package ru.stoloto.support;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactCollector;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.*;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
+import org.apache.maven.shared.dependency.graph.traversal.CollectingDependencyNodeVisitor;
+import org.apache.maven.shared.dependency.graph.traversal.DependencyNodeVisitor;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilder;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
 import ru.stoloto.util.FileParameter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
-@Mojo(name = "generate-docs", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = false)
+@Mojo(name = "generate-docs", defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
+        threadSafe = false,requiresDependencyCollection = ResolutionScope.RUNTIME,requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class ParserMojo extends AbstractMojo {
 
     @Component
@@ -29,10 +35,13 @@ public class ParserMojo extends AbstractMojo {
     @Component
     protected MavenSession mavenSession;
 
-
     @Parameter
     @SuppressWarnings({"unused", "MismatchedReadAndWriteOfArray"})
     private FileParameter file;
+
+    @Parameter
+    @SuppressWarnings({"unused", "MismatchedReadAndWriteOfArray"})
+    private FileParameter docpath;
 
     @Parameter(defaultValue = "UTF-8")
     @SuppressWarnings("unused")
@@ -62,11 +71,30 @@ public class ParserMojo extends AbstractMojo {
             }
         }).explore(projectDir);
 
+
+        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+                        super.visit(n, arg);
+                        Optional supertype = n.getExtendedTypes().stream().findFirst();
+                        //  if (n.getComment().isPresent() && n.isAnnotationPresent(Deprecated.class)) {
+                        n.getFields().forEach(c -> getLog().info(c.getComment().get().getContent()));
+                        // }
+                    }
+                }.visit(JavaParser.parse(file), null);
+            } catch (IOException e) {
+                new RuntimeException(e);
+            }
+        }).explore(docpath.getPath());
     }
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info( "Starting analyze the project..." );
         scanComments(mavenProject.getBasedir());
+
     }
+
 }
